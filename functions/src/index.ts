1065107c049449269e70aa8668bd07f9d9e5902f
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2025-04-07 12:15:37 +0900

    init

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
+import {initializeApp} from "firebase-admin/app";
+import {onDocumentWritten} from "firebase-functions/v2/firestore";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+initializeApp();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const onDocumentWrite = onDocumentWritten(
+  {
+    document: "inquiries/{documentId}",
+    region: "asia-northeast1",
+  },
+  (event) => {
+    const {documentId} = event.params;
+    const data = event.data?.after?.data();
+
+    logger.info(
+      `Document ${documentId} was written with data: ${JSON.stringify(data)}`
+    );
+  }
+);
