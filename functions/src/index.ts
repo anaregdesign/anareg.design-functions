ad0d8de24ad39ced423bf5a231d181464e544133
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2025-04-07 12:47:31 +0900

    init

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,8 @@ import {initializeApp} from "firebase-admin/app";
 import {onDocumentWritten} from "firebase-functions/v2/firestore";
 import * as logger from "firebase-functions/logger";
 
+import * as functions from "firebase-functions/v2";
+
 initializeApp();
 
 export const onDocumentWrite = onDocumentWritten(
@@ -9,12 +11,25 @@ export const onDocumentWrite = onDocumentWritten(
     document: "inquiries/{documentId}",
     region: "asia-northeast1",
   },
-  (event) => {
-    const {documentId} = event.params;
+  async (event) => {
     const data = event.data?.after?.data();
 
-    logger.info(
-      `Document ${documentId} was written with data: ${JSON.stringify(data)}`
-    );
+    const webhookUrl = functions.config().discord.webhook.inquiries;
+
+    try {
+      const response = await fetch(webhookUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+    } catch (error) {
+      logger.error("Error sending webhook", error);
+    }
   }
 );
